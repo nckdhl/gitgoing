20d70f9934553395c7a7d6157584200ae5a1f5cf
Nick <nicholasdahl27@gmail.com>
2020-01-29 22:02:32 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const chalk = require('chalk'); // applies ANSI colors and styles
+const figlet = require('figlet'); // creates large letters with text
+const clear = require('clear'); // removes current output in the console
